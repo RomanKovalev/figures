fe7658361aa3fa98a5574c82210455c211db8b89
Roman Kovalev <3217548+RomanKovalev@users.noreply.github.com@users.noreply.github.com>
2019-07-17 11:00:11 +0300

    Added resizing (with debug code)

--- src/components/boxitem/boxitem.js
+++ src/components/boxitem/boxitem.js
@@ -4,18 +4,23 @@ import './boxitem.css'
 
 export default class Boxitem extends Component {
     state = {
-        left:40,
-        top: 40
+        left: 40,
+        top: 40,
+        width: 200,
+        height: 200,
+        draggable: true,
+        offsetx: 0,
+        offsety: 0,
+        resizer: null
     }
 
     onDragStart = (ev) => {
         console.log('onDragStart')
         let style = window.getComputedStyle(ev.target, null);
-        // ev.dataTransfer.setData("id",)
         ev.dataTransfer.setData("text/plain",
             (parseInt(style.getPropertyValue("left"), 10) - ev.clientX) + ',' + (parseInt(style.getPropertyValue("top"), 10) - ev.clientY));
-        // console.log(ev.dataTransfer.getData("text/plain"))
-        // ev.dataTransfer.setData("dropEffect", "move");
+            ev.dataTransfer.setData("effectAllowed", 'copy')
+            ev.dataTransfer.setDragImage(ev.target, window.outerWidth, window.outerHeight)
     }
 
     onDrag = (ev) => {
@@ -26,17 +31,43 @@ export default class Boxitem extends Component {
     onDrop = (ev) => {
         ev.preventDefault();
         console.log('onDrop')
-        const offset = ev.dataTransfer.getData("text/plain").split(',')
-        // console.log(ev.clientX, ev.clientY)
-        // const id = ev.dataTransfer.getData("id")
-        // console.log(id)
-        // const dm = document.getElementById(id)
-        // console.log(dm)
-        this.setState({left: ev.clientX + parseInt(offset[0], 10) + "px"})
-        this.setState({top: ev.clientY + parseInt(offset[1], 10) + "px"})
+        if (this.state.resizer === 'corner left-top') {
+            this.setState({
+                width: this.state.width - (ev.clientX - this.state.offsetx),
+                height: this.state.height - (ev.clientY - this.state.offsety),
+                left: ev.clientX,
+                top: ev.clientY,
+            })
+            console.log()
+        } else if (this.state.resizer === 'corner right-top') {
+            console.log('corner right-top', ev.clientY, this.state.offsety, this.state.left)
+            this.setState({
+                width: this.state.width - (this.state.offsetx - ev.clientX),
+                top: ev.clientY,
+                height: this.state.height - (ev.clientY - this.state.offsety),
+                left: this.state.left,
+            })
+        } else if (this.state.resizer === 'corner left-bottom') {
+            this.setState({
+                width: this.state.width - (ev.clientX - this.state.offsetx),
+                left: ev.clientX,
+                height: this.state.height - (this.state.offsety - ev.clientY),
+                top: this.state.top
+            })
+        } else if (this.state.resizer === 'corner right-bottom') {
+            this.setState({
+                width: this.state.width - (this.state.offsetx - ev.clientX),
+                height: this.state.height - (this.state.offsety - ev.clientY),
+                top: this.state.top,
+                left: this.state.left,
+            })
+        } else {
+            const offset = ev.dataTransfer.getData("text/plain").split(',')
+            this.setState({ left: ev.clientX + parseInt(offset[0], 10) })
+            this.setState({ top: ev.clientY + parseInt(offset[1], 10) })
+        }
+        this.setState({ 'resizer': null })
 
-        // dm.style.left = ev.clientX + parseInt(offset[0], 10) + "px";
-        // dm.style.top = ev.clientY + parseInt(offset[1], 10) + "px";
         return false;
     }
 
@@ -50,29 +81,68 @@ export default class Boxitem extends Component {
         ev.preventDefault();
     }
 
+    onMouseDown = (ev) => {
+        console.log('onMouseDown')
+        this.setState({
+            offsetx: ev.clientX,
+            offsety: ev.clientY,
+            resizer: ev.target.className,
+            draggable: true,
+        })
+        return false;
+    }
+
+    onMouseUp = (ev) => {
+        ev.preventDefault()
+        console.log('onMouseUp')
+
+
+    }
+
 
     render() {
         return (
             <div className="container-drag">
                 <div className="wrapper"
                     onDragOver={(e) => this.onDragOver(e)}
-                    onDrop={(e) => { this.onDrop(e) }}>
+                    onDrop={(e) => { this.onDrop(e) }}
+                    onMouseUp={(e) => this.onMouseUp(e)}
+                >
                     <div
-                        id="con1"
                         className="area"
-                        draggable="true"
+                        draggable={this.state.draggable}
                         onDragStart={(e) => this.onDragStart(e)}
                         style={
                             {
-                                top : this.state.top,
-                                left : this.state.left
+                                top: this.state.top + 'px',
+                                left: this.state.left + 'px',
+                                width: this.state.width + 'px',
+                                height: this.state.height + 'px',
                             }
                         }
-                    >   
-                        <div className="corner left-top" ></div>
-                        <div className="corner right-top"></div>
-                        <div className="corner left-bottom"></div>
-                        <div className="corner right-bottom"></div>
+                    >
+                        TOP: {this.state.top}<br />
+                        LEFT: {this.state.left}<br />
+                        WIDTH: {this.state.width}<br />
+                        HEIGHT: {this.state.height}<br />
+                        Draggable: {this.state.draggable}<br />
+                        Resizer: {this.state.resizer}<br />
+                        <div
+                            className="corner left-top"
+                            onMouseDown={(e) => this.onMouseDown(e)}
+                        ></div>
+                        <div
+                            className="corner right-top"
+                            onMouseDown={(e) => this.onMouseDown(e)}
+                        ></div>
+                        <div
+                            className="corner left-bottom"
+                            onMouseDown={(e) => this.onMouseDown(e)}
+                        ></div>
+                        <div
+                            className="corner right-bottom"
+                            onMouseDown={(e) => this.onMouseDown(e)}
+                        ></div>
                     </div>
                 </div >
             </div >
