3c2d815c3f39314b4008841be6b23b5f6c717dab
Roman Kovalev <3217548+RomanKovalev@users.noreply.github.com@users.noreply.github.com>
2019-07-17 16:35:08 +0300

    Refactor to different components, proper state (w debug code)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,54 +7,101 @@ import './app.css'
 export default class App extends Component {
 
   state = {
-    left: 40,
-    top: 40,
-    width: 100,
-    height: 200,
-    draggable: true,
-    offsetx: 0,
-    offsety: 0,
+    items: [
+      {
+        id: 1,
+        left: 40,
+        top: 20,
+        width: 100,
+        height: 200,
+        draggable: true,
+        offsetx: 0,
+        offsety: 0,
+      },
+      {
+        id: 2,
+        left: 140,
+        top: 120,
+        width: 200,
+        height: 50,
+        draggable: true,
+        offsetx: 0,
+        offsety: 0,
+      },
+      {
+        id: 3,
+        left: 400,
+        top: 20,
+        width: 100,
+        height: 200,
+        draggable: true,
+        offsetx: 0,
+        offsety: 0,
+      },            
+      {
+        id: 4,
+        left: 190,
+        top: 180,
+        width: 90,
+        height: 100,
+        draggable: true,
+        offsetx: 0,
+        offsety: 0,
+      },
+    ],
     resizer: null
   }
 
   onDrop = (ev) => {
     console.log('onDrop')
-    console.log(this.state.left)
     ev.preventDefault();
+    const id = ev.dataTransfer.getData("id") - 1
+    console.log(this.state.items[id])
+
+    let width, height, top, left
+    width = this.state.items[id].width
+    height = this.state.items[id].height
     if (this.state.resizer === 'corner left-top') {
-      this.setState({
-        width: this.state.width - (ev.clientX - this.state.offsetx),
-        height: this.state.height - (ev.clientY - this.state.offsety),
-        left: ev.clientX,
-        top: ev.clientY,
-      })
+      width = this.state.items[id].width - (ev.clientX - this.state.items[id].offsetx)
+      height = this.state.items[id].height - (ev.clientY - this.state.items[id].offsety)
+      left = ev.clientX
+      top = ev.clientY
     } else if (this.state.resizer === 'corner right-top') {
-      this.setState({
-        width: this.state.width - (this.state.offsetx - ev.clientX),
-        top: ev.clientY,
-        height: this.state.height - (ev.clientY - this.state.offsety),
-        left: this.state.left,
-      })
+      width = this.state.items[id].width - (this.state.items[id].offsetx - ev.clientX)
+      top = ev.clientY
+      height = this.state.items[id].height - (ev.clientY - this.state.items[id].offsety)
+      left = this.state.items[id].left
     } else if (this.state.resizer === 'corner left-bottom') {
-      this.setState({
-        width: this.state.width - (ev.clientX - this.state.offsetx),
-        left: ev.clientX,
-        height: this.state.height - (this.state.offsety - ev.clientY),
-        top: this.state.top
-      })
+      width = this.state.items[id].width - (ev.clientX - this.state.items[id].offsetx)
+      left = ev.clientX
+      height = this.state.items[id].height - (this.state.items[id].offsety - ev.clientY)
+      top = this.state.items[id].top
     } else if (this.state.resizer === 'corner right-bottom') {
-      this.setState({
-        width: this.state.width - (this.state.offsetx - ev.clientX),
-        height: this.state.height - (this.state.offsety - ev.clientY),
-        top: this.state.top,
-        left: this.state.left,
-      })
+      width = this.state.items[id].width - (this.state.items[id].offsetx - ev.clientX)
+      height = this.state.items[id].height - (this.state.items[id].offsety - ev.clientY)
+      top = this.state.items[id].top
+      left = this.state.items[id].left
     } else {
       const offset = ev.dataTransfer.getData("text/plain").split(',')
-      this.setState({ left: ev.clientX + parseInt(offset[0], 10) })
-      this.setState({ top: ev.clientY + parseInt(offset[1], 10) })
+      /** todo make pretty **/
+      left = ev.clientX + parseInt(offset[0], 10)
+      top = ev.clientY + parseInt(offset[1], 10)
     }
-    this.setState({ 'resizer': null })
+
+    this.setState(state => {
+      state.items.map((item, j) => {
+        if (j === id) {
+          item.left = left
+          item.top = top
+          item.width = width
+          item.height = height
+          return item
+        } else {
+          return item;
+        }
+      });
+    });
+    this.setState({ 'resizer': null }) 
     return false;
   }
 
@@ -70,40 +117,58 @@ export default class App extends Component {
     ev.preventDefault()
   }
 
-  onDragStart = (ev) => {
+  onDragStart = (ev, id) => {
     console.log('onDragStart')
     let style = window.getComputedStyle(ev.target, null);
+    console.log((parseInt(style.getPropertyValue("left"), 10) - ev.clientX) + ',' + (parseInt(style.getPropertyValue("top"), 10) - ev.clientY))
     ev.dataTransfer.setData("text/plain",
       (parseInt(style.getPropertyValue("left"), 10) - ev.clientX) + ',' + (parseInt(style.getPropertyValue("top"), 10) - ev.clientY));
     ev.dataTransfer.setDragImage(ev.target, window.outerWidth, window.outerHeight)
+    ev.dataTransfer.setData('id', id)
   }
 
-  onMouseDown = (ev) => {
+  onMouseDown = (ev, id) => {
     console.log('onMouseDown')
-
-    this.setState({
-      offsetx: ev.clientX,
-      offsety: ev.clientY,
-      resizer: ev.target.className,
-      draggable: true,
-    })
+    const offsetx = ev.clientX
+    const offsety = ev.clientY
+    this.setState({'resizer': ev.target.className})
+    id--
+    this.setState(state => {
+      state.items.map((item, j) => {
+        if (j === id) {
+          item.offsetx = offsetx
+          item.offsety = offsety
+          item.draggable = true
+          return item
+        } else {
+          return item;
+        }
+      });
+    });
     return false;
   }
 
   render() {
+    const listItems = this.state.items.map((p) =>
+      <Boxitem
+        state={p}
+        key={p.id}
+        onDragStart={this.onDragStart}
+        onMouseDown={this.onMouseDown}
+      />
+
+    );
     return (
       <div>
+        <button>Add +</button>
+        <button>Clear all</button>
         <div className="container-drag">
           <div className="wrapper"
             onDragOver={(e) => this.onDragOver(e)}
-            onDrop={(e) => this.onDrop(e) }
+            onDrop={(e) => this.onDrop(e)}
             onMouseUp={(e) => this.onMouseUp(e)}
           >
-            <Boxitem
-              state={this.state}
-              onDragStart={ (e) => this.onDragStart(e)}
-              onMouseDown={ (e) =>this.onMouseDown(e)}
-            />
+            {listItems}
           </div>
         </div>
       </div>
--- src/components/boxitem/boxitem.js
+++ src/components/boxitem/boxitem.js
@@ -2,39 +2,41 @@ import React from 'react';
 
 import './boxitem.css'
 
-const Boxitem = ({state, onMouseDown, onDragStart }) => {
-        return (
-            <div
-                className="area"
-                draggable={state.draggable}
-                onDragStart={(e) => onDragStart(e)}
-                style={
-                    {
-                        top: state.top + 'px',
-                        left: state.left + 'px',
-                        width: state.width + 'px',
-                        height: state.height + 'px',
-                    }
+const Boxitem = ({ state, onMouseDown, onDragStart }) => {
+    return (
+        <div
+            className="area"
+            draggable={state.draggable}
+            onDragStart={(e) => onDragStart(e, state.id)}
+            style={
+                {
+                    top: state.top + 'px',
+                    left: state.left + 'px',
+                    width: state.width + 'px',
+                    height: state.height + 'px',
                 }
-            > { state.top}, { state.left}, { state.width}, { state.height}, {state.resizer}
-                <div
-                    className="corner left-top"
-                    onMouseDown={(e) => onMouseDown(e)}
-                ></div>
-                <div
-                    className="corner right-top"
-                    onMouseDown={(e) => onMouseDown(e)}
-                ></div>
-                <div
-                    className="corner left-bottom"
-                    onMouseDown={(e) => onMouseDown(e)}
-                ></div>
-                <div
-                    className="corner right-bottom"
-                    onMouseDown={(e) => onMouseDown(e)}
-                ></div>
-            </div>
-        );
-    }
+            }
+        >
+            {state.top}, {state.left}, {state.width}, {state.height},
+            <h1>{state.id}</h1>
+            <div
+                className="corner left-top"
+                onMouseDown={(e) => onMouseDown(e, state.id)}
+            ></div>
+            <div
+                className="corner right-top"
+                onMouseDown={(e) => onMouseDown(e, state.id)}
+            ></div>
+            <div
+                className="corner left-bottom"
+                onMouseDown={(e) => onMouseDown(e, state.id)}
+            ></div>
+            <div
+                className="corner right-bottom"
+                onMouseDown={(e) => onMouseDown(e, state.id)}
+            ></div>
+        </div>
+    );
+}
 
-    export default Boxitem;
+export default Boxitem;
