c5de8cc0e651f17681ccdc7a55a111605d2692c2
Roman Kovalev <rvk.sft@gmail.com>
2020-09-01 16:15:00 +0300

    deploy

--- backend/src/figures_api/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib import admin
-
-from .views import FigureListAPIView, UpdateFigureAPIView
-
-urlpatterns = [
-    path('list', FigureListAPIView.as_view()),
-    path('update/<int:id>/', UpdateFigureAPIView.as_view()),
-]
