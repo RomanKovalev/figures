e12bea8d06550cc6a0cd3812f210cb68e7b139c0
Roman Kovalev <rvk.sft@gmail.com>
2020-09-01 15:18:13 +0300

    deploy

---
+++ build/precache-manifest.076396c664aa01b56d03ca56c4272255.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960ed4f1d401c1cb275b3a63fa7865bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8be1c7b7ee90cbb6016",
+    "url": "/static/css/main.be75ad61.chunk.css"
+  },
+  {
+    "revision": "49549fcc42f3fcb0fd07",
+    "url": "/static/js/2.64909757.chunk.js"
+  },
+  {
+    "revision": "d8be1c7b7ee90cbb6016",
+    "url": "/static/js/main.3365d6cb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
