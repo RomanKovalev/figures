bb06e94e122c0d29e7523cc3bf60262151226f7d
Roman Kovalev <rvk.sft@gmail.com>
2020-09-01 11:57:07 +0300

    deploy

--- backend/src/figures_api/wsgi.py
+++ backend/src/figures_api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.WSGI_APPLICATION')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fugures_api.settings')
 
 application = get_wsgi_application()
