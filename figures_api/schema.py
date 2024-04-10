c57e747f9358523028f184ebc3a321f97ffc8641
Roman Kovalev <rvk.sft@gmail.com>
2024-04-10 22:30:11 +0300

    intro to graphql mutations

--- figures_api/schema.py
+++ figures_api/schema.py
@@ -8,12 +8,39 @@ class FigureType(DjangoObjectType):
     model = Figure
     fields = '__all__'
 
-class Query(graphene.ObjectType):
+class FiguresQuery(graphene.ObjectType):
   figures = graphene.List(FigureType)
 
   def resolve_figures(self, info, **kwargs):
     return Figure.objects.all()
-  # def resolve_users(self, info, **kwargs):
-  #   return User.objects.all()
 
-schema = graphene.Schema(query=Query)
+class CreateFigure(graphene.Mutation):
+  figure = graphene.Field(FigureType)
+  class Arguments:
+    top = graphene.Int(required=True)
+    left = graphene.Int(required=True)
+    width = graphene.Int(required=True)
+    height = graphene.Int(required=True)
+    draggable = graphene.Boolean(required=True)
+    offsetx = graphene.Int(required=True)
+    offsety = graphene.Int(required=True)
+
+  def mutate(self, info, top, left, width, height, draggable, offsetx, offsety):
+    figure = Figure(
+      left=left,
+      top=top,
+      width = width,
+      height = height,
+      draggable = draggable,
+      offsetx = offsetx,
+      offsety = offsety
+    )
+    figure.save()
+    return CreateFigure(figure=figure)
+
+class FigureMutation(graphene.ObjectType):
+  addFigure = CreateFigure.Field()
+  # update_figure = graphene.Field(FigureType)
+
+
+schema = graphene.Schema(query=FiguresQuery, mutation=FigureMutation)
