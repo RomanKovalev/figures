f835f4b49daa6c7707944c6998fed7e0f7dc118d
Roman Kovalev <rvk.sft@gmail.com>
2024-04-10 12:47:24 +0300

    rollback to old nodejs

--- figures_api/api/urls.py
+++ figures_api/api/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
 
 from django.views.decorators.csrf import csrf_exempt
-from graphene_django.views import GraphQLView
-from figures_api import schema
 
 from .views import FigureListAPIView, UpdateFigureAPIView
 
 urlpatterns = [
     path('list', FigureListAPIView.as_view()),
     path('update/<int:id>/', UpdateFigureAPIView.as_view()),
-    path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
